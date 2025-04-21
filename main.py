da54d1d81867511d423368d671789c52c9acdbcd
Eren Tural <erentur1905@gmail.com>
2025-04-21 04:27:41 +0300

    empty entries code added

--- main.py
+++ main.py
@@ -2,32 +2,34 @@ import tkinter as tk
 
 my_window = tk.Tk()
 my_window.title("BMI Calculator")
-my_window.minsize(500, 500)
+my_window.config(padx=50, pady=50)
 
 height_input = tk.IntVar()
 weight_input = tk.IntVar()
 
 
 height_label = tk.Label(my_window, text="Enter your Height (cm)")
-height_label.place(x=200, y=50)
+height_label.pack()
 
 height_entry = tk.Entry(my_window, textvariable=height_input)
-height_entry.place(x=180, y=75)
+height_entry.pack()
 
 weight_label = tk.Label(my_window, text="Enter your Weight (kg)")
-weight_label.place(x=200, y=100)
+weight_label.pack()
 
 weight_entry = tk.Entry(my_window, textvariable=weight_input)
-weight_entry.place(x=180, y=125)
+weight_entry.pack()
 
 result_label = tk.Label(my_window)
-result_label.place(x=160, y=160)
+result_label.pack()
 
 def calculate():
 
   height_calc = height_input.get() / 100
-  result = (weight_input.get() / (height_calc * height_calc))
-  if result <= 18.4:
+  result = weight_input.get() / height_calc ** 2
+  if height_input.get() == 0 or weight_input.get() == 0:
+      result_label.config(text="Enter both weight and height")
+  elif result <= 18.4:
       result_label.configure(text="Your BMI is: "+ str(result) +  " Underweight")
   elif (result >= 18.5) and (result <= 24.9):
       result_label.configure(text="Your BMI is: "+ str(result) +  " Normal")
@@ -39,7 +41,7 @@ def calculate():
 
 
 calculator_button = tk.Button(my_window, text="Calculate",command=calculate)
-calculator_button.place(x=225, y=200)
+calculator_button.pack()
 
 
 
